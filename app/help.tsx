2da4cd3792e5f0b6804020c82b999b1c970087a3
Ny Antsaniaina <hei.nyantsaniaina@gmail.com>
2026-01-06 10:17:39 +0300

    chore: edit helpstyle

--- app/help.tsx
+++ app/help.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function Help() {
   return (
     <View style={{ flex: 1, padding: 20 }}>
-      <Text style={{ color: "#fff", fontSize: 28 }}>Aide</Text>
-      <Text style={{ color: "#fff", marginTop: 20, fontSize: 16 }}>
+      <Text style={{ fontSize: 28 }}>Aide</Text>
+      <Text style={{ marginTop: 20, fontSize: 16 }}>
         - Le but du jeu est d’éviter les bombes.
         {"\n"}- Appuyez sur une case pour la révéler.
         {"\n"}- Un chiffre indique combien de bombes sont autour.
